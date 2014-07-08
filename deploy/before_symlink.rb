ae1046f481ca7115ddb8961f03c0f7a49d0cb000
Tyler Bird <tyler.bird@gmail.com>
2014-07-08 14:37:38 -0600

    change symlink

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,4 +1,4 @@
 # before_symlink.rb
 
   # symlink the wp-config.php file created by custom chef recipe
-  run "ln -nfs /data/#{app_name}/shared/config/wp-config.php /data/#{app_name}/current/public/wp-config.php"
+  run "ln -nfs /data/#{config.app_name}/shared/config/wp-config.php /data/#{config.app_name}/current/public/wp-config.php"
